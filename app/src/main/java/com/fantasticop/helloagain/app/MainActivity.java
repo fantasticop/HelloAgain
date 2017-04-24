9af4bf3a913b6b117c5756e8686d26c668d0e196
Ikhsan Ramadhan <melontea100@gmail.com>
2017-04-24 23:10:48 +0800

    Initial commit

---
+++ app/src/main/java/com/fantasticop/helloagain/app/MainActivity.java
@@ -0,0 +1,32 @@
+package com.fantasticop.helloagain.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+
+        Button firstButton = (Button) findViewById(R.id.firstButton);
+
+        firstButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                firstTextView.setText("You Clicked");
+
+            }
+        });
+    }
+}
